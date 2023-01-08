4c581a57c8eb80b1d7fc7d2ca7b7ce0b3f182ce0
Ivan-Dell <vanbog@mail.ru>
2023-01-08 20:42:00 +0300

    ChatService uploaded

---
+++ ChatService/ChatClient/MessageClient.cs
@@ -0,0 +1,44 @@
+﻿using ChatService.Models;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    internal class MessageClient
+    {
+        private HubConnection _hubConnection;
+        private int _id;
+
+        public MessageClient(int id)
+        {
+            _id = id;
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(new Uri("http://localhost:5131/hub/messages"))
+                .WithAutomaticReconnect()
+                .Build();
+            _hubConnection.StartAsync().Wait();
+            var connection = _hubConnection.ConnectionId;
+            _hubConnection.On<Message>("MessageReceived", OnMessageReceivedAsync);
+        }
+
+        private void OnMessageReceivedAsync(Message message)
+        {
+            Console.WriteLine($"#{message.FromId} -> {message.ToId} >>> {message.Text}");
+        }
+
+        public Task SendMessageAsync(int toId, string text)
+        {
+            return _hubConnection.InvokeAsync("MessageAll", new Message
+            {
+                FromId = _id,
+                ToId = toId,
+                Text = text
+            });
+        }
+
+    }
+}
---
+++ ChatService/ChatService/Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using ChatService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+
+        private readonly IMessageService _messageService;
+
+        public ClientController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        [Route("connect")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        public ActionResult<int> Connect(string name)
+        {
+            return Ok(_messageService.AddClient(name));
+        }
+
+    }
+}
